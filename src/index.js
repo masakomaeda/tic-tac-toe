99e52ec6ada8901fad9aa9110011f7e9550e2ec7
masakomaeda <masakomaeda126@yahoo.co.jp>
2019-01-21 09:21:28 +0900

    not yet finish

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 
-class Square extends React.Component {
-  constructor() {
-    super()
+class Square extends Component {
+  constructor(props) {
+    super(props)
     this.state = { value:null }
   }
   
   render() {
     return (
-      <button className="square" onClick={() => this.setState({ value: 'x'})}>
+      <button className="square" onClick={() => this.props.onClick()}>
         {this.state.value}
       </button>
     );
@@ -18,8 +18,21 @@ class Square extends React.Component {
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+  
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
+  
   renderSquare(i) {
-    return <Square value={i}/>;
+    return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)} />;
   }
   render() {
     const status = 'Next player: X';
